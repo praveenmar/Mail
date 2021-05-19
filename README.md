eb91fe09d8651a0829b6b0aa3b31e9badea5c521
senthil.sk <senthilk979@gmail.com>
2021-05-19 12:52:01 +0200

    Added files

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple Node App to send mail
+
+**How to send mail from Node JS app deployed in AWS EC2**
+
+Here is the link to the clear description of what to do.
+
+[Medium Post](https://medium.com/@senthilk979/how-to-send-mail-from-node-js-app-deployed-in-aws-ec2-db006be86a85)
+
+Author details:
+
+**Senthil Kumar
+Brussels, Belgium**
+
+
+[Medium](https://senthilk979.medium.com/)
+[Facebook](https://www.facebook.com/senthilk979)
+[Twitter](https://twitter.com/senthilk979)
+[LinkedIn](https://www.linkedin.com/in/senthilk979/)
+[Instagram](https://www.instagram.com/senthilk979/)
+[Email](mailto:t.senthilkumar4@outlook.com)
---
+++ index.js
@@ -0,0 +1,69 @@
+//import modules installed at the previous step. We need them to run Node.js server and send emails
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const path = require('path');
+
+// create a new Express application instance
+const app = express();
+
+//configure the Express middleware to accept CORS requests and parse request body into JSON
+app.use(cors({origin: "*"}));
+app.use(bodyParser.json());
+
+// app.use(express.static(process.cwd()+"/resourcing/public/"));
+
+// Redirect all the other resquests
+app.get('*', (req, res) => {
+    res.status(200);
+    res.send("This is a node js aplication which sends email");
+});
+
+//start application server on port 3000
+app.listen(8888, () => {
+    console.log("The server started on port 8888");
+});
+
+// define a sendmail endpoint, which will send emails and response with the corresponding status
+app.post("/api/sendmail", (req, res) => {
+    console.log("request came");
+    let user = req.body;
+    sendMail(user, (err, info) => {
+        if (err) {
+            console.log(err);
+            res.status(400);
+            res.send({error: "Failed to send email"});
+        } else {
+            console.log("Email has been sent");
+            res.send(info);
+        }
+    });
+});
+
+const sendMail = (user, callback) => {
+    const mailOptions = {
+        from: `"youemail@gmail.com"`,
+        to: user.toMail,
+        cc: user.ccMail,
+        subject: user.subject,
+        html: user.html
+    };
+    if (user.resume !== null && user.resume !== undefined) {
+        mailOptions.attachments = [{
+            filename: user.fileName,
+            path: user.resume
+        }]
+    }
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "youremail@gmail.com",
+            pass: "***********"
+        }
+    });
+
+    transporter.sendMail(mailOptions, callback);
+}
